cf5e98df2a30804a332e79a055cfa05e33f4c9dc
Emanuel M Dominic <emanuel@sun.ac.za>
2020-09-23 00:13:39 +0200

    summary of days released while infectious per traveller

--- wimmy_analysis.R
+++ wimmy_analysis.R
@@ -10,15 +10,19 @@ td1_results <- run_scenario(
   flight_time              = 2/24
 )
 
-td3_results <- run_scenario(
+scenario2 <- run_scenario(
   prevalence               = 0.05,
   quarentine_days          = 3,
   syndromic_sensitivity    = 0.7,
   n_travellers             = 1000,
-  n_sims                   = 1000,
+  n_sims                   = 10000,
   flight_time              = 2/24
 )
 
-td3_results %>% 
+dat1 <- scenario2 %>% 
   group_by(sim) %>% 
-  summarise(sum(days_released_inf, na.rm = T))
+  summarise(sum_days_released_inf = sum(days_released_inf, na.rm = T),
+            trav_vol = first(trav_vol),
+            prevented_boarding = sum(is.na(released_t)),
+            released_travellers = trav_vol - prevented_boarding) %>% 
+  mutate(days_released_inf_per_traveller = sum_days_released_inf/released_travellers)
