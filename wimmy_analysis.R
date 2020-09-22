83312d1a8ca7e27c76a72849191bd52666044b60
Emanuel M Dominic <emanuel@sun.ac.za>
2020-09-22 22:20:57 +0200

    compute total days of infections per simulation

--- wimmy_analysis.R
+++ wimmy_analysis.R
@@ -4,7 +4,7 @@ source('wimmy_functions.R')
 td1_results <- run_scenario(
   prevalence               = 0.05,
   quarentine_days          = 1,
-  syndromic_sensitivity  = 0.7,
+  syndromic_sensitivity    = 0.7,
   n_travellers             = 1000,
   n_sims                   = 1000,
   flight_time              = 2/24
@@ -13,8 +13,12 @@ td1_results <- run_scenario(
 td3_results <- run_scenario(
   prevalence               = 0.05,
   quarentine_days          = 3,
-  syndromic_sensitivity  = 0.7,
+  syndromic_sensitivity    = 0.7,
   n_travellers             = 1000,
   n_sims                   = 1000,
   flight_time              = 2/24
 )
+
+td3_results %>% 
+  group_by(sim) %>% 
+  summarise(sum(days_released_inf, na.rm = T))
