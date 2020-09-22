0d2606af8920c285a8baaf8f8226417798d00549
Matthys <matthysk@gmail.com>
2020-09-22 22:10:33 +0200

    Add wimmy_analysis script

---
+++ wimmy_analysis.R
@@ -0,0 +1,20 @@
+
+source('wimmy_functions.R')
+
+td1_results <- run_scenario(
+  prevalence               = 0.05,
+  quarentine_days          = 1,
+  syndromic_sensitivity  = 0.7,
+  n_travellers             = 1000,
+  n_sims                   = 1000,
+  flight_time              = 2/24
+)
+
+td3_results <- run_scenario(
+  prevalence               = 0.05,
+  quarentine_days          = 3,
+  syndromic_sensitivity  = 0.7,
+  n_travellers             = 1000,
+  n_sims                   = 1000,
+  flight_time              = 2/24
+)
