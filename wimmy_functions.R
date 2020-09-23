a3d8098fbde37f493721b138d32fa838eea54180
Matthys <matthysk@gmail.com>
2020-09-23 16:19:48 +0200

    Add 0% compliance scenario

--- wimmy_functions.R
+++ wimmy_functions.R
@@ -69,34 +69,36 @@ run_partial_compliance_scenario <- function(
   n_compliant     = round(n_travellers * percent_compliant / 100)
   n_non_compliant = n_travellers - n_compliant
   
-  compliant_strategy <- 
-    tibble(
-      pathogen = "SARS-CoV-2",
-      syndromic_sensitivity  = syndromic_sensitivity,
-      pre_board_screening    = NA,
-      post_flight_screening  = TRUE,
-      first_test_delay       = quarentine_days,
-      second_test_delay      = NA,
-      max_mqp                = 14,
-      post_symptom_window    = 7,
-      results_delay          = 1,
-      scenario               = 1
-    )
+  if(percent_compliant != 0)
+  {
+    compliant_strategy <- 
+      tibble(
+        pathogen = "SARS-CoV-2",
+        syndromic_sensitivity  = syndromic_sensitivity,
+        pre_board_screening    = NA,
+        post_flight_screening  = TRUE,
+        first_test_delay       = quarentine_days,
+        second_test_delay      = NA,
+        max_mqp                = 14,
+        post_symptom_window    = 7,
+        results_delay          = 1,
+        scenario               = 1
+      )
   
-  compliant_result <- run_scenario(
-    strategy                 = compliant_strategy,
-    prev_vector               = prev_vector,
-    syndromic_sensitivity    = syndromic_sensitivity,
-    n_travellers             = n_compliant,
-    n_sims                   = n_sims,
-    flight_time              = flight_time
-  ) %>%
+    compliant_result <- run_scenario(
+      strategy                 = compliant_strategy,
+      prev_vector               = prev_vector,
+      syndromic_sensitivity    = syndromic_sensitivity,
+      n_travellers             = n_compliant,
+      n_sims                   = n_sims,
+      flight_time              = flight_time
+    ) %>%
     mutate(compliant = TRUE)
   
-  if (percent_compliant == 100) {
-    return(compliant_result)
+    if (percent_compliant == 100) {
+      return(compliant_result)
+    }
   }
-  
   non_compliant_strategy <- 
     tibble(
       pathogen = "SARS-CoV-2",
@@ -123,6 +125,11 @@ run_partial_compliance_scenario <- function(
       compliant = FALSE,
       idx = idx + n_compliant
     )
+  
+  if(percent_compliant == 0)
+  {
+    return(non_compliant_result)
+  }
   
   full_result = bind_rows(compliant_result, non_compliant_result)
   
